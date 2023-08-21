d99a2ef8b9d37512364596d338daa832b409c6ed
Sunil Besra <sunilbesra28@gmail.com>
2023-08-21 13:05:44 +0530

    changes

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -31,7 +31,7 @@ const Business = () => {
         <h2 className={styles.heading2}>
           You do the business,
           <br className="sm:block hidden" />
-          we'll handle the money.
+          we'll handle the money.Welcom To The Business World.
         </h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
           With the right credit card, you can improve your financial life by
